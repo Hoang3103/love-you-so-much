438f338e46fe9d71c71ea87c80ee16ba904663c3
Hoàng Nguyễn <162253280+Hoang3103@users.noreply.github.com>
2024-04-17 16:11:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const randY = Math.random();
         heart.style.setProperty('--rand-x', randX);
         heart.style.setProperty('--rand-y', randY);
-        heart.style.animationDelay = Math.random() * 3 + 's'; // Tạo delay ngẫu nhiên từ 0 đến 3s
+        heart.style.animationDelay = Math.random() * 3 + 's'; 
         heart.style.opacity = 0;
     });
 
     const textContainer = document.getElementById('text-content');
-    textContainer.style.display = 'none'; // Ẩn chữ khi trang được tải
+    textContainer.style.display = 'none'; 
 
     const playButton = document.getElementById('play-audio-button');
-    let audioPlaying = false; // Biến để theo dõi trạng thái của âm thanh
+    let audioPlaying = false; 
 
     playButton.addEventListener('click', function () {
-        if (!audioPlaying) { // Kiểm tra xem âm thanh có đang phát không
-            playBackgroundAudio(); // Phát âm thanh
-            startAnimation(); // Bắt đầu hiệu ứng
-            audioPlaying = true; // Cập nhật trạng thái của âm thanh
-            playButton.disabled = true; // Vô hiệu hóa nút
+        if (!audioPlaying) { 
+            playBackgroundAudio(); 
+            startAnimation(); 
+            audioPlaying = true; 
+            playButton.disabled = true; 
         }
     });
 
@@ -28,27 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
         const audio = new Audio('0417.MP3');
         audio.play();
         audio.addEventListener('ended', function () {
-            audioPlaying = false; // Cập nhật trạng thái của âm thanh khi nó kết thúc
-            playButton.disabled = false; // Kích hoạt lại nút sau khi âm thanh kết thúc
+            audioPlaying = false; 
+            playButton.disabled = false; 
         });
     }
 
     function startAnimation() {
-        textContainer.style.display = 'block'; // Hiển thị chữ khi bắt đầu phát âm thanh
-        const words = textContainer.textContent.split(/\s+/); // Tách các từ
-        textContainer.textContent = ''; // Xóa nội dung ban đầu
+        textContainer.style.display = 'block'; 
+        const words = textContainer.textContent.split(/\s+/); 
+        textContainer.textContent = ''; 
 
         words.forEach((word, index) => {
             const span = document.createElement('span');
             span.textContent = word + ' ';
-            span.style.opacity = 0; // Ẩn từ ban đầu
+            span.style.opacity = 0; 
             textContainer.appendChild(span);
 
             // Thêm delay cho từng từ
-            const initialDelay = 2500; // Delay lúc bắt đầu là 2 giây
-            const delayBetweenWords = 400; // Delay giữa các từ là 0.4 giây
-            const totalDelay = initialDelay + (index * delayBetweenWords); // Tổng thời gian delay cho từ hiện tại
-            span.style.animationDelay = totalDelay + 'ms'; // Thiết lập thời gian delay cho từng từ
+            const initialDelay = 2500; 
+            const delayBetweenWords = 400; 
+            const totalDelay = initialDelay + (index * delayBetweenWords); 
+            span.style.animationDelay = totalDelay + 'ms'; 
         });
     }
 });
